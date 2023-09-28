3e6fda19915e54d10ef5eff3e2753c39ec688d43
lufer <lufer@ipca.pt>
2023-09-28 18:16:30 +0100

    Inicialização

---
+++ Aula 2/Pessoa.cs
@@ -0,0 +1,33 @@
+﻿
+/*
+ * Classe para descrever uma Pessoa
+ * data
+ * autor
+ * 
+ * */
+
+namespace Aula_2
+{
+    /// <summary>
+    /// Class Pessoa
+    /// </summary>
+    public class Pessoa
+    {
+        #region ESTADO | ATRIBUTOS | VARIAVEIS
+
+        static int idade;           //privado
+        public static string nome;  //publico
+        public static double peso;
+
+        #endregion
+
+        #region COMPORTAMENTO | Beaviour | METHODS
+
+        public static double GetIdade()
+        {
+            return 25;
+        }
+
+        #endregion
+    }
+}
---
+++ Aula 2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * */
+
+using System;
+
+namespace Aula_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Pessoa.nome = "Luis";
+            Pessoa.peso = 12.3;
+
+            Console.WriteLine("Peso:{0}", Pessoa.peso);
+
+            Console.WriteLine("Idade:{0}", Pessoa.GetIdade());
+
+        }
+    }
+
+}

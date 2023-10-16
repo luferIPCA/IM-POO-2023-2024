449b1b1c2f6fd33ac752b337dbf67798987e40bd
lufer <lufer@ipca.pt>
2023-10-16 15:09:01 +0100

    Array Bidimensional & static

--- Aula 5/Program.cs
+++ Aula 5/Program.cs
@@ -59,36 +59,75 @@ namespace Aula_5
 
         #region ABORDAGEM3
         //Variável protegida por classe
+        //Classe "Farmacia" contém os dados
 
+        //static void Main()
+        //{
+        //    //Cria farmácia
+        //    Farmacia f1 = new Farmacia();
+
+        //    //Cria medicamentos
+        //    Medicamento m1 = new Medicamento("Novo",TipoMedi.ANTIBIOTICO, 1);
+        //    Medicamento m2 = new Medicamento("Novo", TipoMedi.ANTIBIOTICO,2);
+        //    Medicamento m3 = new Medicamento("Novo3", TipoMedi.ANALGESICO,3);
+
+        //    //Mostra medicamento
+        //    //Console.WriteLine("Medicamento: Nome: {0} - Tipo: {1}", m1.Nome, m1.Tipo);
+        //    //ou
+        //    Console.WriteLine(m1.ToString());
+
+        //    bool aux = f1.InsereMedicamentoFarmacia(m1);
+        //    aux = f1.InsereMedicamentoFarmacia(m2);
+
+        //    //Mostra todos os medicamentos existentes na farmácia
+        //    f1.ShowMedicamentos();  
+        //    //Console.WriteLine("Farmacia:{0}", f1);    //QUal o resultado?
+
+        //    //Mostra quantos medicamentos existem na farmácia
+        //    Console.WriteLine("Medicamentos na Farmácia:" + f1.QuantosMedicamentos().ToString());
+        //    //ou
+        //    //Usa método externo para mostrar todas as prateleiras
+        //    Frontend.ShowMedicamentosConsole(f1.MedicamentosExistentes, f1.QuantosMedicamentos());
+
+        //    //Farmacia f2 = new Farmacia("Outra farmacia"); //O que acontece?
+
+        //    Console.ReadKey();
+        //}
+        #endregion
+
+
+        #region AORDAGEM4
+        /// <summary>
+        /// Dados estão num array bidimensional, na classe "Armario"
+        /// </summary>
         static void Main()
         {
-            //Cria faarmácia
-            Farmacia f1 = new Farmacia();
-
             //Cria medicamentos
-            Medicamento m1 = new Medicamento("Novo",TipoMedi.ANTIBIOTICO, 1);
-            Medicamento m2 = new Medicamento("Novo", TipoMedi.ANTIBIOTICO,2);
-            Medicamento m3 = new Medicamento("Novo3", TipoMedi.ANALGESICO,3);
+            Medicamento m1 = new Medicamento("Novo", TipoMedi.ANTIBIOTICO, 1);
+            Medicamento m2 = new Medicamento("Novo", TipoMedi.ANTIBIOTICO, 2);
+            Medicamento m3 = new Medicamento("Novo3", TipoMedi.ANALGESICO, 3);
 
             //Mostra medicamento
             //Console.WriteLine("Medicamento: Nome: {0} - Tipo: {1}", m1.Nome, m1.Tipo);
             //ou
             Console.WriteLine(m1.ToString());
 
-            bool aux = f1.InsereMedicamentoFarmacia(m1);
-            aux = f1.InsereMedicamentoFarmacia(m2);
+            /*
+             * Não necessita de criar Armário (Farmácia), pois 
+             * o array é static
+            */
+            Armario.LimpaPrateleiras();
+            Armario.InsereMedicamentoFarmacia(m1);
+            Armario.InsereMedicamentoFarmacia(m2);
+            Armario.InsereMedicamentoFarmacia(m3);
 
-            //Mostra todos os medicamentos existentes na farmácia
-            f1.ShowMedicamentos();  
-            //Console.WriteLine("Farmacia:{0}", f1);    //QUal o resultado?
+            //Mostrar todos os medicamentos existentes nas prateleiras
+            Armario.ShowPrateleiras();
 
-            //Mostra quantos medicamentos existem na farmácia
-            Console.WriteLine("Medicamentos na Farmácia:" + f1.QuantosMedicamentos().ToString());
-            //ou
-            //Usa método externo para mostrar todas as prateleiras
-            Frontend.ShowMedicamentosConsole(f1.MedicamentosExistentes, f1.QuantosMedicamentos());
+            Console.WriteLine("Existe Medicamento: " + Armario.ExisteMedicamentoFarmacia("Novo3", TipoMedi.ANALGESICO));
+            Console.WriteLine("Total com nome {0} = {1} " , "Novo",Armario.QuantosMedicamentosNomeExistemFarmacia("Novo"));
+            Console.WriteLine("Total de {0}: {1}", TipoMedi.ANTIBIOTICO, Armario.QuantosMedicamentosTipoExistemFarmacia(TipoMedi.ANTIBIOTICO));
 
-            //Farmacia f2 = new Farmacia("Outra farmacia"); //O que acontece?
 
             Console.ReadKey();
         }
@@ -96,5 +135,5 @@ namespace Aula_5
 
     }
 
-    
+
 }
